09c1e0dc134e3c7f4394d448b0cbdb84f9f7f071
einoayk <einoayk@hotmail.com>
2019-02-05 21:17:16 +0200

    commit build to backend

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ morgan.token('content', getContent = (req) => {
 
 app.use(cors())
 
+app.use(express.static('build'))
+
 app.use(bodyParser.json())
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
